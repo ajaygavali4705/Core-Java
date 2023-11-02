99562d5fb63881f6d61bf58a987a38477c6216b4
Dell <Dell@DESKTOP-E6QKFAQ>
2023-11-02 11:07:36 +0530

    looping and Branching programs

---
+++ DayTwo/src/com/tnsif/controlstatements/BreakDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.controlstatements;
+
+public class BreakDemo {
+	public static void main(String[] args) {
+
+		for (int i = 5; i < 10; i++) {
+			if (i == 7)
+				break;
+			System.out.println(i);
+		}
+ System.out.println("End of the program");
+	}
+}
